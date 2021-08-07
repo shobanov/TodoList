341ebf5c8efb09040efb656d2715afcf0ea6e5f4
shobanov <shobanoff@gmail.com>
2021-08-07 11:48:39 +0300

    Updates

---
+++ precache-manifest.95eb4e9196f57c5ef0a825c724184736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d5b957de72c6ff32d5d88ba9e294e7",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "d54c2e0af5aeca63c93a",
+    "url": "/TodoList/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "667afc1a3e1448839b5d",
+    "url": "/TodoList/static/js/2.6ceafa7c.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/TodoList/static/js/2.6ceafa7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54c2e0af5aeca63c93a",
+    "url": "/TodoList/static/js/main.854576d7.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
